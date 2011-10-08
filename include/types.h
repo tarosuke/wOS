ce6eacf63c03ce667a9669075406df03b47c30fc
tarosuke <webmaster@tarosuke.net>
2011-10-08 15:48:20 +0900

    マイクロカーネル化に向けて開発準備中

--- include/types.h
+++ include/types.h
@@ -21,10 +21,21 @@ typedef u64 time;
 namespace TIME{
 	static const time infinite = (time)-1LL;
 	static const time continuous = (time)-2LL;
-}
+};
 
 /// talos charactor
 typedef u32 tchar;
+
+
+/// version
+typedef struct{
+	u16 major;	/** 互換性がなくなった時にインクリメント(ただし旧verをサポートすること) */
+	u16 minor;	/** 修正、要素の追加などでインクリメント */
+	u32 release;	/** 実装のリリースでインクリメント */
+}VERSION;
+
+
+
 
 
 #endif
