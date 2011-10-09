a353a64721d8a2a99a753090c1a60f729332d88b
tarosuke <webmaster@tarosuke.net>
2011-10-10 01:09:37 +0900

    デバグ用printfと、Panic、assertを追加

--- kernel/code/debug.cc
+++ kernel/code/debug.cc
@@ -33,7 +33,165 @@ void dputs(const char* s){
 		dputc(*s);
 	}
 }
-void dprintf(const char* format, ...){
 
+
+static inline void Hex(u64 val, char head, int chars){
+	static const char* n = "0123456789abcdef";
+	char buff[20];
+	char* p;
+
+	// 生成
+	p = buff;
+	do{
+		*p++ = n[val & 15];
+		val >>= 4;
+		chars--;
+	}while(val);
+
+	//
+	while(0 < chars--){
+		*p++ = head;
+	}
+
+	//出力
+	for(p--; buff <= p; p--){
+		dputc(*p);
+	}
 }
+
+static void UDec(u64 val, char head, char sign, int chars){
+	char buff[20];
+	char* p;
+
+	// 生成
+	p = buff;
+	do{
+		*p++ = '0' + (val % 10);
+		val /= 10;
+		chars--;
+	}while(val);
+
+	//
+	if(sign == '-'){
+		chars--;
+	}
+	while(0 < chars--){
+		*p++ = head;
+	}
+
+	//出力
+	if(sign == '-'){
+		dputc('-');
+	}
+	for(p--; buff <= p; p--){
+		dputc(*p);
+	}
+}
+
+static inline void Dec(i64 val, char head, int chars){
+	char sign = ' ';
+	if(val < 0){
+		sign = '-';
+		val = -val;
+	}
+	UDec((uint)val, head, sign, chars);
+}
+
+
+void dprintf(const char* f, ...){
+	const munit* p;
+	char h;
+	int n;
+
+	for(p = reinterpret_cast<munit*>((munit)&f), p++; *f; f++){
+		if(*f == '%'){
+			uint longLevel;
+			f++;
+
+			/***** 桁フォーマット取得 */
+			h = ' ';
+			n = 0;
+			if(*f == '0'){
+				h = '0';
+				f++;
+			}
+			while('0' <= *f && *f <= '9'){
+				n *= 10;
+				n += *f++ - '0';
+			}
+
+			for(longLevel = 0; *f == 'l'; f++, longLevel++);
+
+			/***** 出力 */
+			switch(*f){
+				case 0 : return;
+				case 's' :
+					dputs((const char*)*p++);
+					longLevel = 0;
+					break;
+				case 'd' :
+					if(1 < longLevel){
+						Dec(*(i64*)p, h, n);
+						p += 2;
+					}else{
+						Dec((int)*p++, h, n);
+					}
+					break;
+				case 'u' :
+					if(1 < longLevel){
+						UDec(*(u64*)p, h, ' ', n);
+						p += 2;
+					}else{
+						UDec((uint)*p++, h, ' ', n);
+					}
+					break;
+				case 'x' :
+					if(1 < longLevel){
+						Hex(*(u64*)p, h, n);
+						p += 2;
+					}else{
+						Hex((uint)*p++, h, n);
+					}
+					break;
+				case 'c' :
+					dputc((char)*p++);
+					break;
+				default :
+					dputc(*f);
+					break;
+			}
+		}else{
+			dputc(*f);
+		}
+	}
+}
+
+
+void Panic(const char* message){
+	dputs(message);
+#if 3 <= CF_DEBUG_LEVEL
+	u32* s = (u32*)(void*)&message;
+	for(uint i = 0; i < 128; i++, s++){
+		if(!(i & 7)){
+			dprintf("\n%02x: ", i);
+		}
+		dprintf("%08x ", *s);
+	}
+#endif
+	for(;;){
+		asm volatile("hlt");
+	};
+}
+
+
+void __AssertionFailed(const char* file, const unsigned line){
+	dprintf("Assertion fAILED at %s:%u", file, line);
+	Panic("");
+}
+#if 5 <= CF_DEBUG_LEVEL
+void __AssertionPassed(const char* file, const unsigned line){
+	dprintf("Assertion passed at %s:%u\n", file, line);
+}
+#endif
+
 #endif
--- kernel/include/debug.h
+++ kernel/include/debug.h
@@ -10,10 +10,71 @@
 
 #include <config.h>
 
+
 #if CF_DEBUG_LEVEL
+void __AssertionFailed(const char* file, const unsigned line);
+#if 5 <= CF_DEBUG_LEVEL
+void __AssertionPassed(const char* file, const unsigned line);
+#else
+#define __AssertionPassed(a,b)
+#endif
 void dputc(char);
 void dputs(const char*);
 void dprintf(const char*, ...);
+void Panic(const char*);
+#define assert(c) {\
+	if(!(c)){\
+		__AssertionFailed(__FILE__, __LINE__);\
+	}else{\
+		__AssertionPassed(__FILE__, __LINE__);\
+	}\
+}
+#else
+#define dputc(c)
+#define dputs(s)
+#define dprintf(f,...)
+#define Panic(s)
+#define assert(c);
+#endif
+
+#if 2 <= CF_DEBUG_LEVEL
+#define eputc(c) dputc((c))
+#define eputs(s) dputs((s))
+#define eprintf(f, ...) dprintf(f, __VA_ARGS__)
+#else
+#define eputc(c)
+#define eputs(s)
+#define eprintf(f,...)
+#endif
+
+#if 3 <= CF_DEBUG_LEVEL
+#define fputc(c) dputc((c))
+#define fputs(s) dputs((s))
+#define fprintf(f, ...) dprintf(f, __VA_ARGS__)
+#else
+#define fputc(c)
+#define fputs(s)
+#define fprintf(f,...)
+#endif
+
+#if 4 <= CF_DEBUG_LEVEL
+#define gputc(c) dputc((c))
+#define gputs(s) dputs((s))
+#define gprintf(f, ...) dprintf(f, __VA_ARGS__)
+#else
+#define gputc(c)
+#define gputs(s)
+#define gprintf(f,...)
+#endif
+
+#if 5 <= CF_DEBUG_LEVEL
+#define hputc(c) dputc((c))
+#define hputs(s) dputs((s))
+#define hprintf(f, ...) dprintf(f, __VA_ARGS__)
+#else
+#define hputc(c)
+#define hputs(s)
+#define hprintf(f,...)
 #endif
 
 #endif
