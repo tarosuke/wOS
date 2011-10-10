39ea037fe7bde2ed5456793dd190a1e7eb4cd20d
tarosuke <webmaster@tarosuke.net>
2011-10-11 02:08:09 +0900

    設計メモに追記

--- docs/memo.mm
+++ docs/memo.mm
@@ -1,12 +1,12 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1304599254763" ID="ID_34708357" MODIFIED="1304599284851" TEXT="&#x65b9;&#x91dd;">
-<node CREATED="1304599291438" ID="ID_1147356289" MODIFIED="1304599514649" POSITION="right" TEXT="&#x30e6;&#x30fc;&#x30b6;&#x30d7;&#x30ed;&#x30bb;&#x30b9;&#x306b;&#x7af6;&#x5408;&#x306e;&#x89e3;&#x6c7a;&#x3092;&#x3055;&#x305b;&#x306a;&#x3044;">
+<node CREATED="1304599291438" ID="ID_1147356289" MODIFIED="1318266263088" POSITION="right" TEXT="&#x30e6;&#x30fc;&#x30b6;&#x30d7;&#x30ed;&#x30bb;&#x30b9;&#x306b; &#x306a;&#x308b;&#x3079;&#x304f;&#x7af6;&#x5408;&#x306e;&#x89e3;&#x6c7a;&#x3092;&#x3055;&#x305b;&#x306a;&#x3044;">
 <node CREATED="1304599364945" ID="ID_250085320" MODIFIED="1304599390559" TEXT="&#x30e1;&#x30c3;&#x30bb;&#x30fc;&#x30b8;&#x30c9;&#x30ea;&#x30d6;&#x30f3;&#x306b;&#x3088;&#x308b;&#x30d5;&#x30a1;&#x30a4;&#x30d0;&#x30fc;&#x3067;&#x52d5;&#x4f5c;">
 <node CREATED="1304602062980" ID="ID_975902752" MODIFIED="1304602076972" TEXT="&#x30d7;&#x30ed;&#x30bb;&#x30b9;&#x306e;&#x5358;&#x6a5f;&#x80fd;&#x5316;"/>
 <node CREATED="1304602817340" ID="ID_1961743545" MODIFIED="1304602837236" TEXT="&#x30d5;&#x30a1;&#x30a4;&#x30d0;&#x30fc;&#x306f;&#x30e9;&#x30a4;&#x30d6;&#x30e9;&#x30ea;&#x3067;&#x63d0;&#x4f9b;"/>
+<node CREATED="1318266282770" ID="ID_1958225054" MODIFIED="1318266316239" TEXT="&#x3069;&#x3046;&#x3057;&#x3066;&#x3082;&#x5fc5;&#x8981;&#x306a;&#x90e8;&#x5206;&#x306f;&#x30ed;&#x30c3;&#x30af;&#x30d5;&#x30ea;&#x30fc;&#x30d7;&#x30ed;&#x30c8;&#x30b3;&#x30eb;&#x3068;&#x304b;&#x3092;&#x30e9;&#x30a4;&#x30d6;&#x30e9;&#x30ea;&#x3067;&#x63d0;&#x4f9b;"/>
 </node>
-<node CREATED="1304599396205" ID="ID_1930383605" MODIFIED="1304599410944" TEXT="&#x30b9;&#x30ec;&#x30c3;&#x30c9;&#x306f;&#x30b5;&#x30dd;&#x30fc;&#x30c8;&#x3057;&#x306a;&#x3044;"/>
 <node CREATED="1304602231306" ID="ID_1125948337" MODIFIED="1304602251424" TEXT="&#x30e6;&#x30fc;&#x30b6;&#x30d7;&#x30ed;&#x30bb;&#x30b9;&#x306e;&#x54c1;&#x8cea;&#x7dad;&#x6301;"/>
 </node>
 <node CREATED="1304599657466" ID="ID_412926018" MODIFIED="1304599669538" POSITION="right" TEXT="&#x30de;&#x30a4;&#x30af;&#x30ed;&#x30ab;&#x30fc;&#x30cd;&#x30eb;">
@@ -16,6 +16,14 @@
 <node CREATED="1304601952318" ID="ID_341825171" MODIFIED="1304601972693" TEXT="&#x30d7;&#x30ed;&#x30bb;&#x30b9;&#x306e;&#x4ea4;&#x63db;&#x53ef;&#x80fd;&#x6027;"/>
 <node CREATED="1304601991608" ID="ID_1690798264" MODIFIED="1304602005983" TEXT="&#x30d7;&#x30ed;&#x30bb;&#x30b9;&#x306e;&#x9060;&#x9694;&#x52d5;&#x4f5c;&#x53ef;&#x80fd;&#x6027;"/>
 </node>
+<node CREATED="1318266198188" ID="ID_1851583259" MODIFIED="1318266224188" TEXT="&#x3067;&#x3082;&#x30ab;&#x30fc;&#x30cd;&#x30eb;&#x81ea;&#x8eab;&#x306f;&#x30e1;&#x30c3;&#x30bb;&#x30fc;&#x30b8;&#x306e;&#x5185;&#x5bb9;&#x306f;&#x77e5;&#x3089;&#x306a;&#x3044;"/>
+</node>
+<node CREATED="1318265640854" ID="ID_1128288934" MODIFIED="1318265663802" TEXT="&#x30ce;&#x30f3;&#x30d7;&#x30ea;&#x30a8;&#x30f3;&#x30d7;&#x30c6;&#x30a3;&#x30d6;">
+<node COLOR="#000000" CREATED="1318265669533" ID="ID_1513051915" MODIFIED="1318266045536" TEXT="&#x30ab;&#x30fc;&#x30cd;&#x30eb;&#x306b;&#x3044;&#x308b;&#x6642;&#x9593;&#x3092;&#x77ed;&#x304f;&#x3059;&#x308b;">
+<node CREATED="1318265979596" ID="ID_13917368" MODIFIED="1318266086577" TEXT="&#x30e1;&#x30c3;&#x30bb;&#x30fc;&#x30b8;&#x3092;&#x30dd;&#x30fc;&#x30c8;&#x306b;&#x7f6e;&#x3044;&#x3066;&#x30b9;&#x30b1;&#x30b8;&#x30e5;&#x30fc;&#x30eb;&#x3059;&#x308b;&#x3060;&#x3051;"/>
+<node CREATED="1318266104467" ID="ID_703724429" MODIFIED="1318266170101" TEXT="&#x5171;&#x6709;&#x30e1;&#x30e2;&#x30ea;&#x306e;&#x30da;&#x30fc;&#x30b8;&#x306f;&#x30b3;&#x30d4;&#x30fc;&#x3057;&#x306a;&#x3044;&#x3067;&#x5fc5;&#x8981;&#x306b;&#x306a;&#x3063;&#x305f;&#x3089;&#x30e1;&#x30e2;&#x30ea;&#x7a7a;&#x9593;&#x3092;&#x5207;&#x308a;&#x66ff;&#x3048;&#x3066;&#x53d6;&#x5f97;&#x3057;&#x3066;&#x304f;&#x308b;"/>
+</node>
+<node CREATED="1318265858251" ID="ID_925058617" MODIFIED="1318265947712" TEXT="&#x30ab;&#x30fc;&#x30cd;&#x30eb;&#x4e2d;&#x306e;&#x5272;&#x308a;&#x8fbc;&#x307f;&#x306f;&#x623b;&#x308b;&#x3068;&#x304d;&#x306b;&#x30bf;&#x30b9;&#x30af;&#x306e;&#x767b;&#x9332;&#x3060;&#x3051;&#x3057;&#x3066;&#x304a;&#x304f;"/>
 </node>
 </node>
 </node>
