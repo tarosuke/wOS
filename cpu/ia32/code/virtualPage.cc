1faf2a0af9c68360ffd73e5c2362c92969d7bf92
tarosuke <webmaster@tarosuke.net>
2011-10-27 14:32:32 +0900

    Disableに解放するページを仮想ページで指定するというミスがあったので修正

--- cpu/ia32/code/virtualPage.cc
+++ cpu/ia32/code/virtualPage.cc
@@ -77,7 +77,7 @@ void VIRTUALPAGE::Disable(void* start, munit pages){
 	for(munit v(p); v < p + pages; v++){
 		u32& pte(pageTableArray[v]);
 		if(pte & present){
-			REALPAGE::ReleasePage(v);
+			REALPAGE::ReleasePage(pte >> 12);
 		}
 		pte = 0;
 	}
