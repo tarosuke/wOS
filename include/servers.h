592251a52d6f4d485a001d6b22ea2b1b8485ee95
tarosuke <webmaster@tarosuke.net>
2011-10-30 03:15:06 +0900

    些事：サーバID定義にPCIデバイスを追加／タスクヘッダについてのメモを追加

--- include/servers.h
+++ include/servers.h
@@ -1,3 +1,7 @@
+/************************************************************* SERVERTYPE LIST
+ *	Copyright (C) 2011- project talos (http://talos-kernel.sf.net/)
+ *	check LICENSE.txt. If you don't have the file, mail us.
+ */
 
 
 #ifndef _SERVERS_
@@ -9,9 +13,10 @@
 
 
 /** サーバの説明
- * 種別の識別にUUIDを使っているのは各々のベンダが勝手にプロトコルを決めていいようにするため。
- * これは実装のIDではなく、プロトコルのIDである。
- * 従って、中身がどんなに変わろうともプロトコルがアッパーコンパチである限りは同じIDを使うこと。
+ * 各々のベンダが勝手にプロトコルを決めていいようにするためにUUIDを識別に使う
+ * UUIDはgenuuidとかでその場で作れる
+ * これは実装のIDではなく、プロトコルのIDである
+ * 従って、中身がどんなに変わろうともプロトコルがアッパーコンパチである限りは同じID
  */
 
 
@@ -21,6 +26,17 @@ typedef struct{
 	uuid vender;
 	VERSION productVersion;
 }SERVER_DEF;
+
+
+
+/** talos開発チームのvenderID
+ * ベンダもUUIDで識別される
+ * 文字列だと処理が面倒なのでUUIDを使う
+ */
+static const char* VENDER_talos = "3ae3d690-13ad-48a4-bca4-ae4a3e6cbd25";
+
+
+
 
 
 /** DISPLAY SERVER
@@ -40,6 +56,15 @@ static const char* SERVER_Display = "b612916b-0504-489d-a1e0-d46e231244ba";
  * 権限を受け取ったら、セッション情報を開いて(DESKTOP SERVERは独自にリソースを開く権限を持つ)再現する。
  */
 static const char* SERVER_Desktop = "507cdc22-d3b8-4231-ad70-9016ac6e9344";
+
+
+
+/** PCIデバイスのドライバ
+ * arch/(arch)/kernel/pcibus.ccにより探索、起動される
+ */
+static const char* SERVER_PCI_Device = "fba50c55-c731-4908-a98b-65956f8abeb4";
+
+
 
 
 #endif
