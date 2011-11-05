e18a94ea210c77aa87b9d34a985055019a3d6d46
tarosuke <webmaster@tarosuke.net>
2011-11-05 20:06:55 +0900

    32bits互換モードのセグメントを設定

--- arch/oadg/include/arch/segments.h
+++ arch/oadg/include/arch/segments.h
@@ -23,8 +23,10 @@
 #define KDSel	(KCSel + 8)
 #define UCSel	(KDSel + 8)
 #define UDSel	(UCSel + 8)
-#define	TCSel	(UDSel + 8)
-#define	TSSSel	(TCSel + 8)
+#define	UCSel32	(UDSel + 8)
+#define	UDSel32	(UCSel32 + 8)
+#define	TSSSel	(UDSel32 + 8)
+#define	TCSel	UCSel32
 #define GDTLIMIT (TSSSel + 16 * CF_MAX_PROCESSORs)
 #endif
 
