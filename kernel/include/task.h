2e5ce28f157d26a072e1fd99c03c5291315fc00e
tarosuke <webmaster@tarosuke.net>
2011-11-05 16:15:59 +0900

    TASKとCPUの関係(予定)を記述

--- kernel/include/task.h
+++ kernel/include/task.h
@@ -19,10 +19,12 @@ extern "C"{
 
 
 class TASK{
+	friend class CPU;
 public:
 	void Enqueue(MESSAGE*); //TODO:メッセージをタスクのキューに追加してタスクをreadyにする。もし既にタスクがreadyであり、かつメッセージ優先度がタスク優先度より高ければタスク優先度をメッセージ優先度に変更し新しい優先度のレディキューに繋ぎ直す。
 private:
 	QUEUE<MESSAGE> in;
+	CPU* owner;	//現在このタスクを実行しているプロセッサ
 };
 
 
