d80ea24e28dad932e9018fb08cc4185b30e6c5ee
tarosuke <webmaster@tarosuke.net>
2011-11-12 17:03:33 +0900

    ia32/amd64のEXCEPTIONを統合

--- cpu/ia32/code/exception.cc
+++ cpu/ia32/code/exception.cc
@@ -1,7 +1,6 @@
 /*********************************************************** EXCEPTION HANDLER
  *	Copyright (C) 2011- project talos (http://talos-kernel.sf.net/)
  *	check LICENSE.txt. If you don't have the file, mail us.
- *	$Id$
  */
 
 #include <cpu/exception.h>
@@ -53,26 +52,40 @@ extern "C"{
 		}else{
 			EXCEPTION::FRAME frame(*f);
 			dprintf("EXCEPTION(%d:%08x)."INDENT, num, err);
-			dprintf("eip:%p.\n", frame.withErrorCode.ip);
-			dprintf(" cs:%llx.\n", frame.withErrorCode.cs);
-			dprintf("flg:%08llx.\n", frame.withErrorCode.flags);
-			dprintf("esp:%p.\n", frame.withErrorCode.sp);
-			dprintf(" ss:%llx.\n"UNINDENT, frame.withErrorCode.ss);
+			dprintf("ip:%p.\n", frame.withErrorCode.ip);
+			dprintf("cs:%llx.\n", frame.withErrorCode.cs);
+			dprintf("fl:%08llx.\n", frame.withErrorCode.flags);
+			dprintf("sp:%p.\n", frame.withErrorCode.sp);
+			dprintf("ss:%llx.\n"UNINDENT, frame.withErrorCode.ss);
 			assert(false);
 		}
 	}
 	extern u32 __ExceptionTable[CF_MAX_IRQs][4];
 }
 
+#if CF_IA32
 u64 EXCEPTION::vector[systemExceptions + CF_MAX_IRQs]__attribute__((aligned(8)));
 const EXCEPTION::IDTP EXCEPTION::idtp = { (systemExceptions + CF_MAX_IRQs) * 8, (void*)vector };
+#endif
+#if CF_AMD64
+u64 EXCEPTION::vector[systemExceptions + CF_MAX_IRQs][2]__attribute__((aligned(8)));
+const EXCEPTION::IDTP EXCEPTION::idtp = { (systemExceptions + CF_MAX_IRQs) * 16, (void*)vector };
+#endif
 static EXCEPTION exeptionHandler;
 
 EXCEPTION::EXCEPTION(){
 	dputs("exception..." INDENT);
 	for(uint n(0); n < systemExceptions + CF_MAX_IRQs; n++){
 		u64 p((munit)__ExceptionTable[n]);
+#if CF_IA32
 		vector[n] = 0x00008e0000000000ULL | (KCSel << 16) | ((p & 0xffff0000ULL) << 32) | (p & 0xffffULL);
+#endif
+#if CF_AMD64
+		vector[n][0] = 0x00008e0000000000ULL | (KCSel << 16) |
+		((p & 0xffff0000ULL) << 32) |
+		(p & 0xffffULL);
+		vector[n][1] = p >> 32;
+#endif
 	}
 	asm volatile("lidt %0" :: "m"(idtp));
 	dputs(UNINDENT "OK.\n");
--- cpu/ia32/include/cpu/exception.h
+++ cpu/ia32/include/cpu/exception.h
@@ -18,6 +18,7 @@ class EXCEPTION{
 	void operator=(EXCEPTION&);
 public:
 	static const uint systemExceptions = 32;
+#if CF_IA32
 	struct FRAME{
 		u16 es;
 		u16 ds;
@@ -47,11 +48,53 @@ public:
 			}withErrorCode;
 		};
 	}__attribute__((packed));
+#endif
+#if CF_AMD64
+	struct FRAME{
+		u64 r15;
+		u64 r14;
+		u64 r13;
+		u64 r12;
+		u64 r11;
+		u64 r10;
+		u64 r9;
+		u64 r8;
+		u64 rbp;
+		u64 rsi;
+		u64 rdx;
+		u64 rcx;
+		u64 rbx;
+		u64 rax;
+		u64 rdi;
+		union{
+			struct{
+				u64 ip;
+				u64 cs;
+				u64 flags;
+				u64 sp;
+				u64 ss;
+			}noErrorCode;
+			struct{
+				u64 errorCode;
+				u64 ip;
+				u64 cs;
+				u64 flags;
+				u64 sp;
+				u64 ss;
+			}withErrorCode;
+		};
+	}__attribute__((packed));
+#endif
 	EXCEPTION();
 	typedef void (*HANDLER)(u32, FRAME&);
 	static void RegisterHandler(uint num, HANDLER);
 private:
+#if CF_IA32
 	static u64 vector[];
+#endif
+#if CF_AMD64
+	static u64 vector[][2];
+#endif
 	static const struct IDTP{
 		u16 limit; void* table; }__attribute__((packed)) idtp;
 };
