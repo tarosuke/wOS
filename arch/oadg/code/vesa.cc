771997a8b2a0d1d59a8bca64ff28902307d03c2f
tarosuke <webmaster@tarosuke.net>
2012-04-15 15:26:39 +0900

    プロテクトモードのVESAインターフェイス走査コードを追加。

--- arch/oadg/code/vesa.cc
+++ arch/oadg/code/vesa.cc
@@ -70,7 +70,21 @@ extern "C"{
 		u32	maxPixelClock;
 		uchar	res3[189];
 	}__VESA_displayInfo;
+	extern char __kernel_base[];
 };
+
+struct PMIB{
+	char signature[4];
+	u16 entryPoint;
+	u16 initEntryPoint;
+	u16 BIOSDataSel;
+	u16 A0Sel;
+	u16 B0Sel;
+	u16 B8Sel;
+	u16 codeSegSel;
+	char inProtectMode;
+	char checksum;
+}__attribute__((packed));
 
 
 
@@ -79,15 +93,15 @@ static void* Get16(munit p){
 }
 
 void VESA_Check(){
-#if 0
+#if 3 <= CF_DEBUG_LEVEL
 	dputs("enumlating supported display modes...\n");
 	for(u16* m((u16*)Get16(__VESA_InfoBlock.videoModes)); *m != 0xffff; m++){
 		if(0x10d <= (*m & 0x1ff)){
-			dprintf(" %04x\n", *m);
+			dprintf(" %04x", *m);
 		}
 	}
-#endif
-	dprintf(" VESA signature:%04s\n", __VESA_InfoBlock.signature);
+
+	dprintf("\n VESA signature:%04s\n", __VESA_InfoBlock.signature);
 	dprintf("        version:%04x\n", __VESA_InfoBlock.version);
 	dprintf("     OEM string:%s\n", Get16(__VESA_InfoBlock.oemString));
 	dprintf("     venderName:%s\n", Get16(__VESA_InfoBlock.venderName));
@@ -98,5 +112,30 @@ void VESA_Check(){
 	dprintf("         height:%u.\n", __VESA_displayInfo.yResolution);
 	dprintf("            bpp:%u.\n", __VESA_displayInfo.bitPerPixel);
 	dprintf("       BaseAddr:%08lx.\n", __VESA_displayInfo.physBasePtr);
+
+	dputs("scanning protect-mode VESA interface..."INDENT);
+	const PMIB* pm(0);
+	for(u32* p((u32*)&__kernel_base[0xc0000]); p <
+(u32*)&__kernel_base[0xd0000]; p++){
+		if(*p == 0x44494d50){
+			pm = (PMIB*)p;
+			break;
+		}
+	}
+	if(pm){
+		const char* const s((*pm).signature);
+		dprintf("     signeture:%c%c%c%c\n", s[0], s[1], s[2], s[3]);
+		dprintf("    entryPoint:%x\n", (*pm).entryPoint);
+		dprintf("initEntryPoint:%x\n", (*pm).initEntryPoint);
+		dprintf("   BIOSDataSel:%x\n", (*pm).BIOSDataSel);
+		dprintf("         A0Sel:%x\n", (*pm).A0Sel);
+		dprintf("         B0Sel:%x\n", (*pm).B0Sel);
+		dprintf("         B8Sel:%x\n", (*pm).B8Sel);
+		dprintf("    codeSegSel:%x\n", (*pm).codeSegSel);
+		dprintf(" inProtectMode:%x\n", (*pm).inProtectMode);
+	}else{
+		dputs(UNINDENT"not found.\n");
+	}
+#endif
 }
 
